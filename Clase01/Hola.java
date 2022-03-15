a2ba51fd19db58fbcbcd5d6e8cc8aa7f856f6c16
Mariano <66401052+tsaicruz@users.noreply.github.com>
2022-03-14 21:49:51 -0300

    Add files via upload

---
+++ Clase01/Hola.java
@@ -0,0 +1,26 @@
+public class hola
+{
+//La clase principal/ publica del archivo tiene que tener el mismo nombre que el Archivo, en este caso Hola y Hola
+//Solo puede existir un miembro publico por archivo, un archivo puede contener Clases(1 sola publica) Interfaces o enums.
+
+
+public static void main(String[] args)
+{
+// Tema pendiente explicar String[] args.
+
+    System.out.println("Hola Mundo!");
+
+}
+
+
+
+}
+
+
+class Lunes{}
+class Martes{}
+interface Ejecutable{}
+enum Semana{}
+
+
+// en el ejecutar de cmd de windows podemos poner 
